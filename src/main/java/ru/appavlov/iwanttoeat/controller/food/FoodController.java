616f5b0b4d456db7fb7cd4629541809278478eb1
antonpavlov <anton.it.pavlov@gmail.com>
2018-03-07 15:56:10 +0300

    дорабавление в контроллены метода /get по id

--- src/main/java/ru/appavlov/iwanttoeat/controller/food/FoodController.java
+++ src/main/java/ru/appavlov/iwanttoeat/controller/food/FoodController.java
@@ -3,6 +3,7 @@ package ru.appavlov.iwanttoeat.controller.food;
 import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import ru.appavlov.iwanttoeat.model.food.Food;
 import ru.appavlov.iwanttoeat.service.impl.food.FoodService;
@@ -20,5 +21,10 @@ public class FoodController {
     @RequestMapping("/all")
     public List<Food> getFoods() {
         return service.getAll();
+    }
+
+    @RequestMapping("/get")
+    public Food get(@RequestParam("id") long id) {
+        return service.get(id);
     }
 }
--- src/main/java/ru/appavlov/iwanttoeat/controller/product/ProductController.java
+++ src/main/java/ru/appavlov/iwanttoeat/controller/product/ProductController.java
@@ -3,6 +3,7 @@ package ru.appavlov.iwanttoeat.controller.product;
 import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import ru.appavlov.iwanttoeat.model.product.Product;
 import ru.appavlov.iwanttoeat.service.impl.product.ProductService;
@@ -20,6 +21,11 @@ public class ProductController {
     @RequestMapping("/all")
     public List<Product> getFoods() {
         return service.getAll();
+    }
+
+    @RequestMapping("/get")
+    public Product get(@RequestParam("id") long id) {
+        return service.get(id);
     }
 }
 
