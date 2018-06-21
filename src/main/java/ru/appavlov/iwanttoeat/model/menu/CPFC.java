4808654cd2f6764b0aaebec69ea9b17646c75d6d
antonpavlov <anton.it.pavlov@gmail.com>
2018-06-21 16:19:40 +0300

    приведение CPFC к int

--- src/main/java/ru/appavlov/iwanttoeat/model/menu/CPFC.java
+++ src/main/java/ru/appavlov/iwanttoeat/model/menu/CPFC.java
@@ -4,15 +4,22 @@ import lombok.Data;
 
 @Data
 public class CPFC {
-    public double calories;
-    public double proteins;
-    public double fats;
-    public double carbohydrates;
+    public int calories;
+    public int proteins;
+    public int fats;
+    public int carbohydrates;
 
     public CPFC() {
     }
 
     public CPFC(double calories, double proteins, double fats, double carbohydrates) {
+        this.calories = (int) Math.ceil(calories);
+        this.proteins = (int) Math.ceil(proteins);
+        this.fats = (int) Math.ceil(fats);
+        this.carbohydrates = (int) Math.ceil(carbohydrates);
+    }
+
+    public CPFC(int calories, int proteins, int fats, int carbohydrates) {
         this.calories = calories;
         this.proteins = proteins;
         this.fats = fats;
